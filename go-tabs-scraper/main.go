efc54b64285956a1f1045f9880ee161883cd6529
ia <isaac.ardis@gmail.com>
2022-05-21 08:59:40 -0700

    refactor scraper func to show error context, and retry <=3
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- go-tabs-scraper/main.go
+++ go-tabs-scraper/main.go
@@ -32,6 +32,7 @@ func main() {
 	spanStart := flag.Uint64("span-start", math.MaxInt64, "Start of block query span (default=latest-1")
 	spanRange := flag.Uint64("span-range", 1, "Duration in blocks of block query span (default=1)")
 	spanRate := flag.Float64("span-rate", 1.0, "Rate of blocks query within span (default=1.0)")
+	retryLimit := flag.Int("retry-limit", 3, "How many times to re-attempt erroring requests (default=3)")
 	url := flag.String("url", "http://127.0.0.1:8545", "Ethclient endpoint URL")
 	flag.Parse()
 
@@ -74,95 +75,109 @@ func main() {
 	signer := types.NewLondonSigner(chainID)
 
 	for blockN := *spanStart; blockN <= *spanStart+*spanRange; blockN++ {
-		blockFilePath := filepath.Join(*datadir, fmt.Sprintf("block_%v", blockN))
-		if fi, err := os.Stat(blockFilePath); err == nil || os.IsExist(err) {
-			log.Println("Data exists, skipping", fi.Name())
-			continue
-		}
 
-		if rand.Float64() > *spanRate {
-			continue
+		try := 0
+		err := scrapeBlock(datadir, blockN, spanRate, client, bkgrnd, signer)
+		for ; err != nil && try < *retryLimit; try++ {
+			err = scrapeBlock(datadir, blockN, spanRate, client, bkgrnd, signer)
 		}
-		log.Printf("Querying block.n=%d\n", blockN)
-
-		// Get the block
-		bl, err := client.BlockByNumber(bkgrnd, big.NewInt(int64(blockN)))
 		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Get the miner's balance at the parent block
-		parentN := big.NewInt(int64(blockN) - 1)
-		minerBalanceAtParent, err := client.BalanceAt(bkgrnd, bl.Coinbase(), parentN)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Bundle it up in our app data type.
-		ap := lib.AppBlock{
-			Header:               bl.Header(),
-			TxesN:                bl.Transactions().Len(),
-			MinerBalanceAtParent: minerBalanceAtParent,
-			AppTxes:              []lib.AppTx{},
-
-			TABWithMiner:       new(big.Int).Set(minerBalanceAtParent),
-			TABWithMinerPretty: lib.PrettyBalance(minerBalanceAtParent),
-
-			TABWithoutMiner:       new(big.Int),
-			TABWithoutMinerPretty: new(big.Float),
-		}
-
-		uniqueSenders := make(map[common.Address]bool)
-
-		for txi, tx := range bl.Transactions() {
-
-			// Extract the 'from' address using the presumed signer.
-			msg, err := tx.AsMessage(signer, bl.BaseFee())
-			if err != nil {
-				log.Fatalln(err)
-			}
-			from := msg.From()
-			if _, ok := uniqueSenders[from]; ok {
-				continue
-			} else {
-				uniqueSenders[from] = true
-			}
-
-			// Get her balance.
-			bal, err := client.BalanceAt(bkgrnd, from, parentN)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			prettyBal := lib.PrettyBalance(bal)
-			ap.TABWithoutMiner.Add(ap.TABWithoutMiner, bal)
-			ap.TABWithoutMinerPretty.Add(ap.TABWithoutMinerPretty, prettyBal)
-
-			ap.TABWithMiner.Add(ap.TABWithMiner, bal)
-			ap.TABWithMinerPretty.Add(ap.TABWithMinerPretty, prettyBal)
-
-			// Bundle it up nice in our app data type.
-			at := lib.AppTx{
-				CTransaction:          tx,
-				Index:                 txi,
-				From:                  from,
-				BalanceAtParent:       bal,
-				BalanceAtParentPretty: prettyBal,
-			}
-			ap.AppTxes = append(ap.AppTxes, at)
-		}
-
-		// Extraction logic done for this block.
-
-		// Persist the data.
-
-		j, err := json.MarshalIndent(ap, "", "    ")
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		if err := ioutil.WriteFile(blockFilePath, j, os.ModePerm); err != nil {
-			log.Fatalln(err)
+			log.Fatalln("[scrape block]", err)
 		}
 	}
+}
+
+func scrapeBlock(datadir *string, blockN uint64, spanRate *float64, client *ethclient.Client, bkgrnd context.Context, signer types.Signer) (error) {
+	blockFilePath := filepath.Join(*datadir, fmt.Sprintf("block_%v", blockN))
+	if fi, err := os.Stat(blockFilePath); err == nil || os.IsExist(err) {
+		log.Println("Data exists, skipping", fi.Name())
+		return nil
+	}
+
+	if rand.Float64() > *spanRate {
+		return nil
+	}
+
+	log.Printf("Querying block.n=%d\n", blockN)
+
+	// Get the block
+	bl, err := client.BlockByNumber(bkgrnd, big.NewInt(int64(blockN)))
+	if err != nil {
+		return fmt.Errorf("[block] %w", err)
+	}
+
+	// Get the miner's balance at the parent block
+	parentN := big.NewInt(int64(blockN) - 1)
+	minerBalanceAtParent, err := client.BalanceAt(bkgrnd, bl.Coinbase(), parentN)
+	if err != nil {
+		return fmt.Errorf("[miner balance] %w", err)
+	}
+
+	// Bundle it up in our app data type.
+	ap := lib.AppBlock{
+		Header:               bl.Header(),
+		TxesN:                bl.Transactions().Len(),
+		MinerBalanceAtParent: minerBalanceAtParent,
+		AppTxes:              []lib.AppTx{},
+
+		TABWithMiner:       new(big.Int).Set(minerBalanceAtParent),
+		TABWithMinerPretty: lib.PrettyBalance(minerBalanceAtParent),
+
+		TABWithoutMiner:       new(big.Int),
+		TABWithoutMinerPretty: new(big.Float),
+	}
+
+	uniqueSenders := make(map[common.Address]bool)
+
+	for txi, tx := range bl.Transactions() {
+
+		// Extract the 'from' address using the presumed signer.
+		msg, err := tx.AsMessage(signer, bl.BaseFee())
+		if err != nil {
+			return fmt.Errorf("[as message] %w", err)
+		}
+		from := msg.From()
+		if _, ok := uniqueSenders[from]; ok {
+			continue
+		} else {
+			uniqueSenders[from] = true
+		}
+
+		// Get her balance.
+		bal, err := client.BalanceAt(bkgrnd, from, parentN)
+		if err != nil {
+			return fmt.Errorf("[tx balance] %w", err)
+		}
+
+		prettyBal := lib.PrettyBalance(bal)
+		ap.TABWithoutMiner.Add(ap.TABWithoutMiner, bal)
+		ap.TABWithoutMinerPretty.Add(ap.TABWithoutMinerPretty, prettyBal)
+
+		ap.TABWithMiner.Add(ap.TABWithMiner, bal)
+		ap.TABWithMinerPretty.Add(ap.TABWithMinerPretty, prettyBal)
+
+		// Bundle it up nice in our app data type.
+		at := lib.AppTx{
+			CTransaction:          tx,
+			Index:                 txi,
+			From:                  from,
+			BalanceAtParent:       bal,
+			BalanceAtParentPretty: prettyBal,
+		}
+		ap.AppTxes = append(ap.AppTxes, at)
+	}
+
+	// Extraction logic done for this block.
+
+	// Persist the data.
+
+	j, err := json.MarshalIndent(ap, "", "    ")
+	if err != nil {
+		return fmt.Errorf("[json] %w", err)
+	}
+
+	if err := ioutil.WriteFile(blockFilePath, j, os.ModePerm); err != nil {
+		return fmt.Errorf("[write file] %w", err)
+	}
+	return nil
 }
